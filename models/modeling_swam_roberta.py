2b3c0293ed0ed7ccabce8f7b805c94476d144334
Jeremiah <1442943357@qq.com>
2022-06-17 23:11:09 +0800

    tmp update

---
+++ models/modeling_swam_roberta.py
@@ -0,0 +1,96 @@
+import torch
+import torch.nn as nn
+import importlib
+from transformers.models.roberta.modeling_roberta import RobertaPreTrainedModel, RobertaModel, RobertaClassificationHead
+from typing import List, Optional, Tuple, Union
+from .swam import SWAM, SWAMClassificationHead
+from torch.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
+from transformers.modeling_outputs import SequenceClassifierOutput
+class SWAMRobertaForSequenceClassification(RobertaPreTrainedModel):
+    _keys_to_ignore_on_load_missing = [r"position_ids"]
+    def __init__(self, config, *args, **kwargs):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+        self.config = config
+
+        self.model_args = kwargs.pop('model_args', None)
+        self.roberta = RobertaModel(config, add_pooling_layer=False)
+        self.swam = SWAM(config)
+        self.classifier = SWAMClassificationHead(config)
+        self.post_init()
+    def forward(
+        self,
+        input_ids: Optional[torch.LongTensor] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        input_ids_0: Optional[torch.LongTensor] = None,
+        attention_mask_0: Optional[torch.FloatTensor] = None,
+        input_ids_1: Optional[torch.LongTensor] = None,
+        attention_mask_1: Optional[torch.FloatTensor] = None,
+        token_type_ids: Optional[torch.LongTensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        head_mask: Optional[torch.FloatTensor] = None,
+        inputs_embeds: Optional[torch.FloatTensor] = None,
+        labels: Optional[torch.LongTensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        potential_input_list = [input_ids, input_ids_0, input_ids_1]
+        potential_attention_list = [attention_mask, attention_mask_0, attention_mask_1]
+        input_ids = [x for x in potential_input_list if x is not None]
+        attention_mask = [x for x in potential_attention_list if x is not None]
+        num_input_ids = len(input_ids)
+        batch_size = input_ids[0].size(0)
+        input_ids = torch.concat(input_ids, dim=0)
+        attention_mask = torch.concat(attention_mask, dim=0)
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=True,
+            return_dict=return_dict,
+        )
+        hidden_states = outputs.hidden_states
+        word_embeddings = hidden_states[0]
+        last_hidden_state = outputs.last_hidden_state
+        swam_outputs, weights = self.swam(word_embeddings, last_hidden_state, attention_mask)
+        logits = self.classifier(swam_outputs)
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (labels.dtype == torch.long or labels.dtype == torch.int):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+
+        if not return_dict:
+            raise ValueError("please implement the following block")
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states if self.model_args.return_hidden_states else None,
+            attentions=outputs.attentions,
+        )
---
+++ models/swam.py
@@ -0,0 +1,41 @@
+import torch.nn as nn
+import torch
+class SWAM(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.hidden_size = config.hidden_size
+        self.output_size = 1 # a score for each word
+        self.embedding_transform = nn.Linear(self.hidden_size, self.output_size)
+        self.last_hidden_transform = nn.Linear(self.hidden_size, self.output_size)
+        self.activation = nn.Tanh()
+        self.get_prob = nn.Softmax(dim=-1)
+    def forward(self, word_embeddings, last_hidden_state, attention_masks):
+        batch, seq_len, _ = last_hidden_state.shape
+        weights = self.embedding_transform(word_embeddings) + self.last_hidden_transform(last_hidden_state)
+        weights = self.activation(weights)
+        weights = weights.view(batch, seq_len) 
+        weights = weights - (attention_masks==0).clone().detach()*10000
+        weights = self.get_prob(weights)# get probs along seq_len dimension
+        swam_outputs = last_hidden_state * weights.unsqueeze(2)
+        swam_outputs = torch.sum(swam_outputs, dim=1)
+        return (swam_outputs, weights)
+class SWAMClassificationHead(nn.Module):
+    """Head for training."""
+
+    def __init__(self, config):
+        super().__init__()
+        self.dense = nn.Linear(config.hidden_size*2, config.hidden_size)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.activation = nn.GELU()
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.outputproj = nn.Linear(config.hidden_size, config.num_labels)
+
+    def forward(self, features, **kwargs):
+        x = self.dropout(features)
+        x = self.activation(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.outputproj(x)      
+        return x
